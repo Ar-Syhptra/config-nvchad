8de5719f7a83ea665011d8d2f47faa7b03009870
Arkansyahputra <theark338@gmail.com>
2025-08-30 17:11:19 +0700

    feat: menambahkan mapping baru untuk resize window dan kontrol terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "<C-\\>", "<C-\\><C-n>")
 map("n", "<Leader>qq", ":qa!<CR>", { desc = "Quit Neovim" })
 map("n", "<Leader>cg", "<cmd> cd ~/AppData/Local/nvim <cr>", { desc = "Open Neovim config folder" })
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
@@ -21,13 +22,12 @@ map("n", "wj", "<C-w>j")
 map("n", "wl", "<C-w>l")
 
 -- Resize window
-map("n", "<C-S-h>", "<C-w><")
-map("n", "<C-S-l>", "<C-w>>")
-map("n", "<C-S-k>", "<C-w>+")
-map("n", "<C-S-j>", "<C-w>-")
+map("n", "<C-w-h>", "<C-w><")
+map("n", "<C-w-l>", "<C-w>>")
+map("n", "<C-w-k>", "<C-w>+")
+map("n", "<C-w-j>", "<C-w>-")
 
 -- Diagnostics
 map("n", "<C-j>", function()
-	vim.diagnostic.goto_next()
+  vim.diagnostic.goto_next()
 end, opts)
-
