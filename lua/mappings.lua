dd9fb24249fd625d35fb76635d7525a8d10b6c67
Arkansyahputra <theark338@gmail.com>
2025-08-30 17:15:57 +0700

    fix: merubah kode mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -22,10 +22,10 @@ map("n", "wj", "<C-w>j")
 map("n", "wl", "<C-w>l")
 
 -- Resize window
-map("n", "<C-w-h>", "<C-w><")
-map("n", "<C-w-l>", "<C-w>>")
-map("n", "<C-w-k>", "<C-w>+")
-map("n", "<C-w-j>", "<C-w>-")
+map("n", "<C-S-h>", "<C-w><")
+map("n", "<C-S-l>", "<C-w>>")
+map("n", "<C-S-k>", "<C-w>+")
+map("n", "<C-S-j>", "<C-w>-")
 
 -- Diagnostics
 map("n", "<C-j>", function()
